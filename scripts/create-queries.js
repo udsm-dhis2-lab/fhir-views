53a59f64997c6bfe1bea93a2743f79cfc5702a80
yuzalsif <liteit68@gmail.com>
2025-02-03 09:33:55 +0300

    feat: Add support to accomodate query from mappings

--- scripts/create-queries.js
+++ scripts/create-queries.js
@@ -69,6 +69,152 @@ const startDate = "2024-11-01";
 const endDate = "2024-11-30:23:59:59";
 
 async function processMappings(datastoreKeyData) {
+  // datastoreKeyData.forEach((datastoreKey) => {
+  //   const mapping = datastoreKey?.value;
+  //   if (mapping && mapping?.params) {
+  //     const dataElementId = mapping.dataElement.id;
+  //     let query = `SELECT en.organization_id,\n`;
+
+  //     query += `  '${dataElementId}' AS "${dataElementId}",\n`;
+
+  //     let hasGender = false;
+  //     let hasAgeGroup = false;
+  //     mapping.params.forEach((param, index) => {
+  //       hasGender = param.gender ? true : false;
+  //       const gender = param.gender;
+  //       const co = param.co;
+  //       const ageType = param.ageType;
+  //       const startAge = param.startAge;
+  //       const endAge = param.endAge;
+  //       hasAgeGroup = ageType && startAge && endAge ? true : false;
+  //       query +=
+  //         ` COUNT(*) ` +
+  //         (hasGender || hasAgeGroup
+  //           ? ` FILTER ( WHERE ${
+  //               hasGender
+  //                 ? "pt.gender='" +
+  //                   (gender === "M" ||
+  //                   gender === "ME" ||
+  //                   gender.toLowerCase() === "male"
+  //                     ? "male"
+  //                     : "female") +
+  //                   "'"
+  //                 : ""
+  //             } ${hasGender && hasAgeGroup ? "AND" : ""} ${
+  //               hasAgeGroup
+  //                 ? "pt.birth_date >= (CURRENT_DATE - INTERVAL '" +
+  //                   startAge +
+  //                   (ageType.toLowerCase() === "years"
+  //                     ? " YEAR "
+  //                     : ageType.toLowerCase() === "months"
+  //                     ? " MONTH "
+  //                     : " DAY ") +
+  //                   "')" +
+  //                   " AND " +
+  //                   "pt.birth_date <= (CURRENT_DATE - INTERVAL '" +
+  //                   endAge +
+  //                   (ageType.toLowerCase() === "years"
+  //                     ? " YEAR "
+  //                     : ageType.toLowerCase() === "months"
+  //                     ? " MONTH "
+  //                     : " DAY ") +
+  //                   "')"
+  //                 : ""
+  //             })`
+  //           : "");
+  //       query += ` AS "${co}"${
+  //         index < mapping.params.length - 1 ? "," : ""
+  //       } \n`;
+  //     });
+  //     let icdCodes = [];
+  //     let loincOrderCodes = [];
+  //     let loincObsCodes = [];
+
+  //     if (mapping.icdMappings && mapping.icdMappings.length > 0) {
+  //       icdCodes = mapping.icdMappings.map((icdMapping, index) => {
+  //         return icdMapping.code;
+  //       });
+  //     }
+
+  //     if (mapping.loincMappings && mapping.loincMappings.length > 0) {
+  //       loincOrderCodes = mapping.loincMappings.map((mappingItem, index) => {
+  //         return mappingItem.code;
+  //       });
+  //     }
+
+  //     if (mapping.loincObsMappings && mapping.loincObsMappings.length > 0) {
+  //       loincObsCodes = mapping.loincObsMappings.map((mappingItem, index) => {
+  //         return mappingItem.code;
+  //       });
+  //     }
+  //     query += ` FROM encounter_flat en \n`;
+
+  //     // Be careful when choosing right vs left join
+  //     if (icdCodes && icdCodes.length > 0) {
+  //       query += `RIGHT JOIN condition_flat cond ON en.id = cond.encounter_id \n`;
+  //       query += `AND cond.code IN ('${icdCodes.join("','")}') \n`;
+  //     }
+  //     if (loincOrderCodes.length > 0) {
+  //       query += `RIGHT JOIN diagnosticreport_flat drep ON en.id = drep.encounter_id \n`;
+  //       query += `AND drep.code IN ('${loincOrderCodes.join("','")}') \n`;
+  //     }
+
+  //     if (mapping.queries && mapping.queries.length > 0) {
+  //       mapping.queries.forEach((queryItem, index) => {
+  //         const leftSide = queryItem.leftSideQuery?.value;
+  //         const operator = queryItem.operator;
+  //         const rightSide = queryItem.rightSideQuery?.value;
+
+  //         if (leftSide && operator && rightSide !== undefined) {
+  //           const leftField =
+  //             leftSide.table && leftSide.code
+  //               ? `${leftSide.table}.${leftSide.code}`
+  //               : leftSide.code;
+
+  //           let rightValue;
+  //           if (queryItem.rightSideQuery.type === "tableField") {
+  //             rightValue =
+  //               rightSide.table && rightSide.code
+  //                 ? `${rightSide.table}.${rightSide.code}`
+  //                 : rightSide.code;
+  //           } else if (queryItem.rightSideQuery.type === "primitiveValue") {
+  //             if (typeof rightSide === "string") {
+  //               rightValue = `'${rightSide}'`;
+  //             } else if (typeof rightSide === "number") {
+  //               rightValue = rightSide;
+  //             } else if (typeof rightSide === "boolean") {
+  //               rightValue = rightSide ? "TRUE" : "FALSE";
+  //             }
+  //           }
+
+  //           if (leftField && operator && rightValue !== undefined) {
+  //             query += `AND ${leftField} ${operator} ${rightValue} \n`;
+  //           }
+  //         }
+  //       });
+  //     }
+  //     if (loincObsCodes.length > 0) {
+  //       query += ` RIGHT JOIN observation_flat obs ON obs.encounter_id = en.encounter_id \n`;
+  //       query += `AND obs.val_concept_code IN ('${loincObsCodes.join(
+  //         "','"
+  //       )}') \n`;
+  //     }
+
+  //     query += `LEFT JOIN patient_flat pt ON pt.id = en.patient_id \n `;
+
+  //     query +=
+  //       loincOrderCodes.length > 0
+  //         ? `AND drep.effective_datetime >='${startDate}' AND drep.effective_datetime <='${endDate}' \n`
+  //         : `AND en.period_start >= '${startDate}' AND en.period_start <= '${endDate}' \n`;
+
+  //     query += ` GROUP BY `;
+  //     query += icdCodes && icdCodes.length > 0 ? `cond.code,` : "";
+  //     query += hasGender ? `pt.gender,` : "";
+  //     query += ` en.organization_id;`;
+  //     console.log(query);
+  //   }
+  // });
+
   const queries = [];
   datastoreKeyData.forEach((datastoreKey) => {
     const mapping = datastoreKey?.value;
@@ -165,6 +311,9 @@ async function processMappings(datastoreKeyData) {
       let conditions = [];
 
       if (mapping.queries && mapping.queries.length > 0) {
+        console.log(
+          "**********************************************WE HAVE QUERIES HERE********************************************************"
+        );
         mapping.queries.forEach((queryItem, index) => {
           const leftSide = queryItem.leftSideQuery?.value;
           const operator = queryItem.operator;
